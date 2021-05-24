bda68ed8247ad133dd68491ea7b08a50c744cd17
Tifenn <tifenn.queudet@gmail.com>
2021-05-24 12:30:36 +0200

    close mobile menu on click outside

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,25 @@
-import React, {useState} from 'react'
+import React, {useEffect} from 'react'
 import logo from '../images/logo.svg'
 import hamburger from '../images/icon-hamburger.svg'
 import iconClose from '../images/icon-close-menu.svg'
 
 const Header = ({isMenuOpen, setIsMenuOpen}) => {
+    const handleDocumentClick = (e) => {
+        if(isMenuOpen) {
+            setIsMenuOpen(false)
+        }
+    }
+
+    useEffect(() => {
+        if(isMenuOpen) {
+            document.addEventListener('click', handleDocumentClick);
+            return () => {
+                document.removeEventListener('click', handleDocumentClick)
+            } 
+        }
+
+    }, [isMenuOpen])
+
     return (
         <>
             <header className="absolute w-full flex items-center justify-between px-5 py-8">
@@ -14,7 +30,7 @@ const Header = ({isMenuOpen, setIsMenuOpen}) => {
                 {isMenuOpen && (
                     <nav className="z-50 absolute w-9/12 ml-10 mt-56 modal">
                         <ul className="bg-white shadow-2xl rounded-2xl py-3">
-                            <li className="cursor-pointer py-3 px-5 border-b border-gray-200">About</li>
+                            <li className="cursor-pointer py-3 px-5 border-b border-gray-200">About</li>  
                             <li className="cursor-pointer py-3 px-5 border-b border-gray-200">Discover</li>
                             <li className="cursor-pointer py-3 px-5">Get Started</li>
                         </ul>
